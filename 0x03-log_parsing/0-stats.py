278b0948b765fec6b87d9be74151a494cd68e593
RachyHassan <rachyhassan@github.com>
2024-05-16 13:52:30 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,13 +5,15 @@ A script that reads stdin line by line and computes metrics
 
 import sys
 
+
 def compute_metrics(lines):
     """
     A function that computes the metrics
     """
     total_size = 0
-    status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    
+    status_counts = {200: 0, 301: 0, 400: 0, 401:
+                     0, 403: 0, 404: 0, 405: 0, 500: 0}
+
     for line in lines:
         parts = line.split()
         if len(parts) == 7 and parts[3].isdigit() and parts[4].isdigit():
@@ -19,8 +21,8 @@ def compute_metrics(lines):
             status_code = int(parts[5])
             if status_code in status_counts:
                 status_counts[status_code] += 1
-    
     return total_size, status_counts
+
 
 def print_statistics(total_size, status_counts):
     """
@@ -30,6 +32,7 @@ def print_statistics(total_size, status_counts):
     for status_code, count in sorted(status_counts.items()):
         if count > 0:
             print(f"{status_code}: {count}")
+
 
 def main():
     """
@@ -46,6 +49,7 @@ def main():
     except KeyboardInterrupt:
         total_size, status_counts = compute_metrics(lines)
         print_statistics(total_size, status_counts)
+
 
 if __name__ == "__main__":
     main()
