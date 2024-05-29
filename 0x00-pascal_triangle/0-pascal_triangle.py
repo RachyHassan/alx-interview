d4608752da304162ccfdeecf3a4a7094ad7865c8
Rachy <RachyHassan@github.com>
2024-05-29 22:21:08 +0100

    0-nqueens.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,10 +12,25 @@ def pascal_triangle(n):
         return []
 
     triangle = []
+    # n = 3
     for i in range(n):
-        row = [1] * (i + 1)
+        row = [1] * (i + 1) # [1] * (3 + 1) = [1, 1, 1, 1]
+        print (row)
+        # range(2, 3)
         for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j] 
+                    # triangle[3 -1][2 - 1] + triangle[3 - 1][2]
+                    # triangle[2][1] + triangle[2][2]
+            # row[2] = 2 + 1 = 3
+            print (3) 
         triangle.append(row)
+        # i = 0: triangle = [[1]]
+        # i = 1: triangle = [[1],[1, 1]]
+        # i = 2: triangle = [[1], [1, 1], [1, 2, 1]]
+        # i = 3: triangle = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]
+        
+
+    print(triangle)
+
 
     return triangle
---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import sys
+
+def print_solution(queens):
+    """Print the board in the required format."""
+    solution = []
+    for i in range(len(queens)):
+        solution.append([i, queens[i]])
+    print(solution)
+
+def is_safe(queens, n, row, col):
+    """Check if it's safe to place a queen at row, col."""
+    for i in range(col):
+        if queens[i] == row or \
+           queens[i] - i == row - col or \
+           queens[i] + i == row + col:
+            return False
+    return True
+
+def solve_nqueens(queens, col, n):
+    """Use backtracking to find all solutions."""
+    if col == n:
+        print_solution(queens)
+        return
+
+    for i in range(n):
+        if is_safe(queens, n, i, col):
+            queens[col] = i
+            solve_nqueens(queens, col + 1, n)
+            queens[col] = -1
+
+def check_input(args):
+    """Check the command-line arguments."""
+    if len(args) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(args[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return n
+
+def main():
+    """Main function to run the N-Queens solver."""
+    n = check_input(sys.argv)
+    queens = [-1 for _ in range(n)]
+    solve_nqueens(queens, 0, n)
+
+if __name__ == "__main__":
+    main()
